03a059983472246bd5c46978c2f96f4e932d036c
carlos.pasquali.dev <carlos.pasquali.dev@gmail.com>
2024-09-26 08:40:22 -0300

    update csv utilities

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,3 @@ import { CSVUtilities } from "./CSVUtilities";
 
 export { CSVUtilities } from "./CSVUtilities";
 export { oldGenerateCSV } from "./old/generateCsv";
-
-const csvUtils = new CSVUtilities(
-  "D:/Development/personal-projects/csv-utilities/cord_19_embeddings_2022-06-02.csv",
-  ","
-);
-
-(async () => {
-  await csvUtils.generateJsonFile("output.json");
-})();
